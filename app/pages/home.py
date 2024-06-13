173000ce84e2c97bb465e56f44f5a0e5c89bc86a
Fernando Golcaves <fernando_jcg22@hotmail.com>
2024-06-13 19:13:52 +0000

    Creat a home.py page

---
+++ app/pages/home.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+def main():
+    st.title('Página Inicial')
+    st.write('Bem-vindo ao meu aplicativo!')
+    # Aqui você pode adicionar mais conteúdo conforme necessário
+
+if __name__ == '__main__':
+    main()
