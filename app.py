4386af62d933176c4a3e23f4094f4a999e254402
Fernando Golcaves <fernando_jcg22@hotmail.com>
2024-07-02 20:36:17 +0000

    App.py - Add the last pages predictor and analysis

--- app.py
+++ app.py
@@ -4,12 +4,16 @@ from app_pages.multipage import MultiPage
 # load pages scripts
 from app_pages.home_page import home_page_body
 from app_pages.predict_price import page_predict_price_ml_body
+from app_pages.analysis import page_analysis_body
+from app_pages.predictor import page_price_predictor_body
 
 app = MultiPage(app_name="Heritage Housing Ames, USA")  # Create an instance of the app
 
 # Add your app pages here using .add_page()
 app.add_page("🏠 Project Summary", home_page_body)
 app.add_page("💰 Predict Price", page_predict_price_ml_body)
+app.add_page("📊 Analysis Prices", page_analysis_body)
+app.add_page("📈 Predictor Price", page_price_predictor_body)
 
 app.run()  # Run the app
 
