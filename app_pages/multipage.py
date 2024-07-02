58fb8906e589acbc9e5b16606aaddc3ccba064cf
Fernando Golcaves <fernando_jcg22@hotmail.com>
2024-07-02 18:59:43 +0000

    z

--- app_pages/multipage.py
+++ app_pages/multipage.py
@@ -5,16 +5,21 @@ class MultiPage:
         self.pages = []
         self.app_name = app_name
 
-        st.set_page_config(
-            page_title=self.app_name,
-            page_icon="🏘️",
-            layout="wide",
-            initial_sidebar_state="expanded",
-            background_color="#000000",  # Black background
-            primary_color="#FFFFFF",  # White text
-            text_color="#FFFFFF",  # White text
-            font="Helvetica Neue"
-        )
+        st.markdown("""
+        <style>
+        streamlit-app, streamlit-app-header, streamlit-app-content {
+            background-color: #000000;
+            color: #FFFFFF;
+            font-family: Helvetica Neue;
+        }
+        streamlit-app-header {
+            border-bottom: 1px solid #FFFFFF;
+        }
+        streamlit-app-content {
+            padding: 1rem;
+        }
+        </style>
+        """, unsafe_allow_html=True)
 
     def add_page(self, title, func) -> None:
         self.pages.append({"title": title, "function": func})
