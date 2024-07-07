d69c6c0a44a99879c5dccefcb881469d157b8992
Fernando Golcaves <fernando_jcg22@hotmail.com>
2024-07-07 16:04:01 +0000

    Project_Hypothesis - Correting H1 h2 h3

--- app_pages/page_project_hypothesis.py
+++ app_pages/page_project_hypothesis.py
@@ -1,22 +1,22 @@
 import streamlit as st
 
 def page_project_hypothesis_body():
-    st.write("### Ames Housing Price Predictor Hypotheses and Validation")
+    st.write("### Heritage Housing Ames, USA Hypotheses and Validation")
 
     # Hypothesis 1
     st.success(
-        f"**H1 - The primary purpose of this project is to provide a tool that enables users to predict the potential sale price of a property in Ames, Iowa.**\n"
-        f"* **Correct.** From the project's purpose and motivation, we found that the client wants to estimate the sale price for several inherited properties in Ames, Iowa, USA.\n\n"
+        f"**H1 - The overall quality (OverallQual) of a property significantly impacts its sale price in Ames, Iowa.**\n"
+        f"* **Correct.** From the feature importance analysis and correlation studies, we found that OverallQual has a strong positive correlation with Sale Price.\n\n"
     )
 
     # Hypothesis 2
     st.success(
-        f"**H2 - The project's dataset represents a record of approximately 1500 real estate sales in Ames, Iowa.**\n"
-        f"* **Correct.** We validated this hypothesis by accessing the dataset at Kaggle, where it is hosted by Code Institute.\n\n"
+        f"**H2 - Properties with larger ground-level living areas (GrLivArea) tend to command higher sale prices in Ames, Iowa.**\n"
+        f"* **Correct.** Our analysis revealed a strong positive correlation between GrLivArea and Sale Price, indicating that larger living areas are associated with higher sale prices.\n\n"
     )
 
     # Hypothesis 3
     st.success(
-        f"**H3 - The client wants to understand the relationship between a property's attributes/features and its sale price.**\n"
-        f"* **Correct.** We found that the client expects data visualizations that illustrate the correlation between these variables and the sale prices.\n"
+        f"**H3 - The age of a property (YearBuilt) influences its sale price, with newer constructions generally commanding higher prices in Ames, Iowa.**\n"
+        f"* **Correct.** Our trend analysis and regression models showed that YearBuilt has a significant positive impact on Sale Price, with newer properties tend to have higher sale prices.\n"
     )
